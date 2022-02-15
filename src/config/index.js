ce289fde5f801e887da8c062b0759f4403b7a2ea
SantiagoPortilla <santiagopor123xd@gmail.com>
2022-02-15 01:30:01 -0500

    coreccion

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,10 @@
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 
-dotenv.config()
+dotenv.config();
 
 const config = {
-    mongoUri = process.env.MONGO_URI,
-    port = process.env.PORT
-} 
+    mongoUri: process.env.MONGO_URI,
+    port: process.env.PORT,
+};
 
 module.exports = config;
